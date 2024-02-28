5c705c34ba26576c02c9edcf37d652752cb88d9f
ahmetcan99 <acerleblebici@gmail.com>
2024-02-28 12:56:36 +0300

    created post model and updated admin page

--- djangoWebApp/blog/views.py
+++ djangoWebApp/blog/views.py
@@ -1,30 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from .models import Post
 # Create your views here.
 
-posts =[
-    {
-        "author": "Can",
-        "title": "My Post",
-        "content": "My First Post",
-        "datePosted": "27 February 2024"
-    },
-    {
-        "author": "Ada",
-        "title": "Her Post",
-        "content": "Her First Post",
-        "datePosted": "28 February 2024"
-    }
-    
-]
-
-
-
+# This is the home page view
 def home(request):
     
     context = {
-        "posts": posts
+        "posts": Post.objects.all()
     }
     
     return render(request, "blog/home.html", context)
