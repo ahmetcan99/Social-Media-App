692738c44b6c1e770f75d2a1116156a85631ae84
ahmetcan99 <acerleblebici@gmail.com>
2024-03-01 16:23:51 +0300

    implemented profile update and author profile picutre on posts

--- djangoWebApp/users/views.py
+++ djangoWebApp/users/views.py
@@ -3,8 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .forms import UserRegisterForm
-
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 def register(request):
     if request.method == "POST":
@@ -24,4 +23,22 @@ def logoutView(request):
 
 @login_required
 def profile(request):
-    return render(request, "users/profile.html")
+    if request.method == "POST":
+        userForm = UserUpdateForm(request.POST, instance=request.user)
+        profileForm = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        
+        if userForm.is_valid() and profileForm.is_valid():
+            userForm.save()
+            profileForm.save()
+            messages.success(request, f"Your account has been updated!")
+            return redirect("profile")  
+    else:
+        userForm = UserUpdateForm(instance=request.user)
+        profileForm = ProfileUpdateForm(instance=request.user.profile)
+    
+    context = {
+        "userForm": userForm,
+        "profileForm": profileForm
+    }
+    
+    return render(request, "users/profile.html", context)
