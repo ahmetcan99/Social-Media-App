31f5e9ad908304cc2cbdcd3d88fb80f4804b8abb
ahmetcan99 <acerleblebici@gmail.com>
2024-03-01 12:37:06 +0300

    Implemented login/logout feature and created a basic profile page

--- djangoWebApp/djangoWebApp/urls.py
+++ djangoWebApp/djangoWebApp/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth import views as authViews
 from django.urls import path, include
 from users import views as userViews
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("register/", userViews.register, name="register"),
+    path("login/", authViews.LoginView.as_view(template_name = "users/login.html"), name="login"),
+    path("logout/", userViews.logoutView, name="logout"),
+    path("profile/", userViews.profile, name="profile"),
     path('', include("blog.urls")),
 ]
--- djangoWebApp/users/views.py
+++ djangoWebApp/users/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import UserRegisterForm
 
@@ -9,9 +11,17 @@ def register(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get("username")
-            messages.success(request, f"Account created for {username}!")
-            return redirect("blogHome")
+            messages.success(request, f"Your account has been crated! You are now able to log in")
+            return redirect("login")
     else:
         form = UserRegisterForm()
     return render(request, "users/register.html", {"form": form})
+
+
+def logoutView(request):
+    logout(request)
+    return render(request, "users/logout.html")
+
+@login_required
+def profile(request):
+    return render(request, "users/profile.html")
